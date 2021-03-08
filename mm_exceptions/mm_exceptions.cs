f32475b853a113e361c08ce4938657fd400a9724
srilekharegul <79085242+srilekharegul@users.noreply.github.com>
2021-03-08 15:48:48 +0530

    Update mm_exceptions.cs

--- mm_exceptions/mm_exceptions.cs
+++ mm_exceptions/mm_exceptions.cs
@@ -3,21 +3,22 @@ using System.Runtime.Serialization;
 
 namespace mm_exceptions
 {
-    [Serializable]
-    public class MovieManagementException : Exception
+    [Serializable]//Can be Serialized
+    public class MovieManagementException : Exception//Inherits exception
     {
         public MovieManagementException()
         {
         }
 
+        //Can change the Exception message for base Exception
         public MovieManagementException(string message) : base(message)
         {
         }
-
+        //Can change the Exception message for derived Exception
         public MovieManagementException(string message, Exception innerException) : base(message, innerException)
         {
         }
-
+        //Derived exeption can be serialized
         protected MovieManagementException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
